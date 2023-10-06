ee662284962a838f4b7d5581684ed460e655c220
Maria <98313650+MariaMasyk@users.noreply.github.com>
2023-10-06 19:20:52 +0300

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
+const container = document.querySelector('.gallery');
+container.insertAdjacentHTML('afterbegin',createMarkup(galleryItems));
+container.addEventListener('click', onImageClick);
+function createMarkup(arr){
+    return arr.map(({preview, original,description}) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      width="300"
+    />
+  </a>
+</li>
+`).join('')
+}
+function onImageClick(event) {
+    event.preventDefault();
+    const target = event.target;
+    if(target.classList.contains('gallery__image')) {
+        const originalImageUrl = target.dataset.source;
+        modal = basicLightbox.create('<img srs = "${galeryItem.original}" width = "800" height = "600">');
+        modal.show();
+        modallsOpen = true;
+    }
+    document.addEventListener('keydown',keyDown);
+    }
+
+
