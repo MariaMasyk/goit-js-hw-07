aaacca9b215b0f5a72c0a2fd4d4399e812a4a950
Maria <98313650+MariaMasyk@users.noreply.github.com>
2023-10-08 23:05:22 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+function createMarkup(arr){
+    return arr.map(({preview, original,description}) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      width="300"
+    />
+    
+  </a>
+</li>
+`).join('')
+}
+const markup = createMarkup(galleryItems);
+container.innerHTML = markup;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  fadeSpeed: 250,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionsPosition: 'bottom',
+});
